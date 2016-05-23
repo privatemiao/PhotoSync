aa7c0c56cded2c20d9f75d18863026293fc916ba
Mel Liu <privatemiao@gmail.com>
2016-05-23 17:14:02 +0800

    TODO test use or not use ionicPlatform.ready for IOS

--- mobile/www/js/win32/controllers.js
+++ mobile/www/js/win32/controllers.js
@@ -1,21 +1,22 @@
 angular.module('starter.controllers')
 
-.controller('SyncController', function($scope) {
-	$scope.variables = {
-		appName : 'Photo Sync',
-		images : (function() {
-			var imgs = [];
-			for (var i = 0; i < 103; i++) {
-				imgs.push({
-					src : 'img/50x50.png'
-				});
-			}
-			return imgs;
-		})(),
-		platform : 'loadding...',
-		model : 'loadding...'
-	};
+.controller('SyncController', function($ionicPlatform, $scope) {
+	$ionicPlatform.ready(function(){ //TODO test use or not use ionicPlatform.ready for IOS
+		$scope.variables = {
+				appName : 'Photo Sync',
+				images : (function() {
+					var imgs = [];
+					for (var i = 0; i < 103; i++) {
+						imgs.push({
+							src : 'img/50x50.png'
+						});
+					}
+					return imgs;
+				})(),
+				platform : ionic.Platform.platform()
+			};
 
+});
 }).controller('ViewController', function($scope) {
 
 }).controller('SettingController', function($scope) {
