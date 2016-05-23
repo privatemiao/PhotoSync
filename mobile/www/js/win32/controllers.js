522765391615b7422639dd727d9b7e7e0b21897b
Mel Liu <privatemiao@gmail.com>
2016-05-23 22:24:42 +0800

    a

--- mobile/www/js/win32/controllers.js
+++ mobile/www/js/win32/controllers.js
@@ -1,22 +1,20 @@
 angular.module('starter.controllers')
 
 .controller('SyncController', function($ionicPlatform, $scope) {
-	$ionicPlatform.ready(function(){ //TODO test use or not use ionicPlatform.ready for IOS
-		$scope.variables = {
-				appName : 'Photo Sync',
-				images : (function() {
-					var imgs = [];
-					for (var i = 0; i < 103; i++) {
-						imgs.push({
-							src : 'img/50x50.png'
-						});
-					}
-					return imgs;
-				})(),
-				platform : ionic.Platform.platform()
-			};
+	$scope.variables = {
+		appName : 'Photo Sync',
+		images : (function() {
+			var imgs = [];
+			for (var i = 0; i < 103; i++) {
+				imgs.push({
+					src : 'img/50x50.png'
+				});
+			}
+			return imgs;
+		})(),
+		platform : ionic.Platform.platform()
+	};
 
-});
 }).controller('ViewController', function($scope) {
 
 }).controller('SettingController', function($scope) {
