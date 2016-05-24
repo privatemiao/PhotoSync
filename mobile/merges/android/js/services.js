e2c8d4bc7da41eae53645c01add8b6b682fee621
Mel Liu <privatemiao@gmail.com>
2016-05-24 14:48:28 +0800

    a

--- mobile/merges/android/js/services.js
+++ mobile/merges/android/js/services.js
@@ -1,1 +1,46 @@
-angular.module('starter.services');
+angular.module('starter.services').factory('FileService', function($q) {
+	return {
+		getParentDirectory : function(path) {
+			var deferred = $q.defer();
+			window.resolveLocalFileSystemURL(path, function(entity) {
+				entity.getParent(function(result) {
+					deferred.resolve(result);
+				}, function(error) {
+					deferred.reject(error);
+				})
+			}, function(error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		},
+		getEntriesAtRoot : function() {
+			var deferred = $q.defer();
+			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(entry) {
+				var directoryReader = entry.root.createReader();
+				directoryReader.readEntries(function(entries) {
+					deferred.resolve(entries);
+				}, function(error) {
+					deferred.reject(error);
+				});
+			}, function(error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		},
+		getEntries : function(path) {
+			var deferred = $q.defer();
+			resolveLocalFileSystemURL(path, function(entry) {
+				var directoryReader = entry.createReader();
+				directoryReader.readEntries(function(result) {
+					deferred.resolve(result);
+				}, function(error) {
+					deferred.reject(error);
+				});
+			}, function(error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		}
+
+	};
+});
