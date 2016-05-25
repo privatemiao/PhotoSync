106d8f5cc2bedda4ff1fc56983575bf2d8b7d450
Mel Liu <privatemiao@gmail.com>
2016-05-25 09:35:02 +0800

    a

--- mobile/merges/ios/js/controllers.js
+++ mobile/merges/ios/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('SyncController', function($ionicPlatform, $scope) {
+.controller('SyncController', function($ionicPlatform, $scope, PhotoService) {
 	$scope.variables = {
 		appName : 'Photo Sync',
 		images : (function() {
@@ -12,8 +12,12 @@ angular.module('starter.controllers')
 			}
 			return imgs;
 		})(),
-		platform : ionic.Platform.platform()
+		photos : []
 	};
+
+	PhotoService.getLocalPhotos($scope.variables.photos).then(function() {
+		console.log($scope.variables.photos);
+	});
 
 }).controller('ViewController', function($scope) {
 
--- mobile/merges/ios/js/services.js
+++ mobile/merges/ios/js/services.js
@@ -1,1 +1,16 @@
-angular.module('starter.services');
+angular.module('starter.services').factory('PhotoService', function($q) {
+	return {
+		getLocalPhotos : function(photos) {
+			var deferred = $q.defer();
+			photos.length = 0;
+			CamerRoll.getPhotos(function(photo) {
+				if (photo) {
+					photos.push(photo);
+				} else {
+					deferred.resolve();
+				}
+			});
+			return deferred.promis;
+		}
+	};
+})
