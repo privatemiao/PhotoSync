fe06d8ba574237bc8b3225d610898a8160b4d139
Mel Liu <privatemiao@gmail.com>
2016-05-26 14:51:00 +0800

    a

--- mobile/merges/ios/js/services.js
+++ mobile/merges/ios/js/services.js
@@ -105,13 +105,12 @@ angular.module('starter.services').factory('PhotoService', function($q) {
 		},
 		checkThumbnailExist : function(fileIdWithExt) {
 			var deferred = $q.defer();
-			
 			requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
 		        fileSystem.root.getFile(cordova.file.dataDirectory + fileIdWithExt, { create: false }, function(){
 		        	console.log('exist')
 		        	deferred.resolve(true);
 		        }, function(){
-		        	console.log('not exist')
+		        	console.log('not exist', cordova.file.dataDirectory + fileIdWithExt);
 		        	deferred.resolve(false);
 		        });
 		    }, function(evt){
@@ -128,3 +127,8 @@ angular.module('starter.services').factory('PhotoService', function($q) {
 // data-ng-src="file:///Users/mel/Library/Developer/CoreSimulator/Devices/28D1EB4D-F4A7-42E9-87D6-5A2CD340809D/data/Containers/Data/Application/E5BE2452-DB6F-40A6-B2CD-DC25ED4AADF6/Library/NoCloud//106E99A1-4F6A-45A2-B320-B0AD4A8E8473.jpg"
 // alt="" width="100%" height="90px"
 // src="file:///Users/mel/Library/Developer/CoreSimulator/Devices/28D1EB4D-F4A7-42E9-87D6-5A2CD340809D/data/Containers/Data/Application/E5BE2452-DB6F-40A6-B2CD-DC25ED4AADF6/Library/NoCloud//106E99A1-4F6A-45A2-B320-B0AD4A8E8473.jpg">
+//file:///Users/mel/Library/Developer/CoreSimulator/Devices/28D1EB4D-F4A7-42E9-87D6-5A2CD340809D/data/Containers/Data/Application/47BFDC28-187B-4959-8BE2-82C92F2EFA33/Library/NoCloud/B84E8479-475C-4727-A4A4-B77AA9980897.JPG
+
+
+
+
